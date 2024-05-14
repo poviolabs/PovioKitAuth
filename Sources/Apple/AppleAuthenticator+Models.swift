ef38881431b8edcbd76b4cb4fda1edd76ccde89f
Borut Tomazin <borut@povio.com>
2024-05-14 12:13:42 +0200

    Address PR comment.

--- Sources/Apple/AppleAuthenticator+Models.swift
+++ Sources/Apple/AppleAuthenticator+Models.swift
@@ -22,12 +22,16 @@ public extension AppleAuthenticator {
     public let nameComponents: PersonNameComponents?
     public let email: Email
     public let expiresAt: Date
-
+    
+    /// User full name represented by `givenName` and `familyName`
     public var name: String? {
-      let fullName = [nameComponents?.givenName, nameComponents?.familyName]
-        .compactMap { $0 }
-        .joined(separator: " ")
-      return !fullName.isEmpty ? fullName : nil
+      guard let givenName = nameComponents?.givenName else {
+        return nameComponents?.familyName
+      }
+      guard let familyName = nameComponents?.familyName else {
+        return givenName
+      }
+      return "\(givenName) \(familyName)"
     }
   }
   
